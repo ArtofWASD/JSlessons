178907c1260f581ca52eab048782ecbbccfef2da
Leonid <chunlee@mail.ru>
2020-03-24 16:39:17 +0300

    первый комит

---
+++ js/script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+
+//console.log(4/0);
+//console.log('string'*8);
+//let something;
+//console.log(something);
+var obj = {};
+let persone = {
+    name: "John",
+    age:25,
+    isMarried: false
+}
+console.log(persone["name"]);
+let arr =['plum.jpg','orange.jpg','apple.bmp'];
+
+console.log(arr[0]);
+
+//alert("Hello world");
+
+//let answer = confirm("do you Retard ?");
+
+//console.log(answer);
+
+// let answer = +prompt("do you Retard?", "Да");
+
+// console.log(typeof(answer));
+// console.log("arr" + "- object ");
+// console.log(4 + "- object ");
+
+// let incr = 10;
+// let decr = 10;
+
+// console.log(incr++);
+// console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = false;
+let isClose = false;
+console.log(isChecked || !isClose);
