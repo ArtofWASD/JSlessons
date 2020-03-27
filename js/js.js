0123f85803e7681e55bee7228e7c1023f157ea39
Leonid <chunlee@mail.ru>
2020-03-27 12:04:31 +0300

    Тестовое задание 2

---
+++ js/js.js
@@ -0,0 +1,8 @@
+//alert("Hello")
+// let answer = confirm("Are you here ?")
+
+// console.log(answer);
+
+let answer = prompt("Have you 19 yera's old", "No");
+
+console.log(typeof(answer));
--- Тестовое задание/script.js
+++ Тестовое задание/script.js
@@ -1,15 +1,17 @@
-let money = prompt("Ваш бюджет на месяц?");
-let time = prompt("Введите дату в формате YYYY-MM-DD");
-console.log(money,time);
-
+var now = new Date();
+let money = prompt('Ваш бюджет на месяц?');
+let time = prompt('Введите дату в формате YYYY-MM-DD', now);
+let expenses1 = prompt('Введите обязательную статью расходов в этом месяце?');
+let expenses2 = prompt('Во сколько обойдётся?');
+// let optionalExpenses1 = prompt('Введите обязательную статью расходов в этом месяце?');
+// let ptionalExpenses2 = prompt('Во сколько обойдётся?');
+let income = [];
 let appData = {
-    money:'50000',
-    time:'2020-03-24',
-    expenses:"Введите обязательную статью расходов в этом месяце",
-    optionalExpenses:'',
-    income:'',
+    money,
+    time,
+    expenses: {expenses1, expenses2},
+    // optionalExpenses,
     savings:false
-
 }
-
-alert(money/30);
+console.log(appData);
+alert("Ваш бюджет на день " + appData.money/30);
